681fba2fcee38f4e97a81b507e6fc8536cc611e7
Virginia Ritonga <virginia.p.m@mail.ugm.ac.id>
2022-12-30 06:28:48 +0700

    style: create page layout

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
+import "../styles/Checkout.css";
 
 function Checkout() {
   const [data, setData] = useState(null);
-  const [termsAccepted, setTermsAccepted] = useState(false);
+  const [setTermsAccepted] = useState(false);
 
   function handleTermsChange(event) {
     setTermsAccepted(event.target.checked);
@@ -23,85 +24,91 @@ function Checkout() {
     <div>
       {data ? (
         <>
-          <div className="delivery">
-            <p>Delivery</p>
-            <p>
-              {data.delivery} - {data.deliveryFee}
-            </p>
-            <p>Tickets available by {data.deliveryDate}</p>
-            <p>
-              These mobile tickets will be transfered directly to you from a
-              trusted seller. We'll email your instructions on how to accept
-              them on the original ticket provider's mobile app.
-            </p>
-          </div>
-
-          <div className="payment">
-            <div>
-              <input type="radio" value="Visa" name="cardInfo" />
-              <p>Visa - {data.creditCardNumber}</p>
-              <p>
-                {data.creditCardName} | exp. {data.creditCardExpiration}
-              </p>
-              <span>Edit | Delete</span>
-              <p>Security Code</p>
-              <input type="password" name="password" />
-              <p>3-digits on back of card</p>
-            </div>
-            <div>Add New Card</div>
-            <div>
-              <p>Or Pay With</p>
-              <div>
-                By using a digital wallet and continuing past this page, you
-                have read and are accepting the{" "}
-                <a href={data.termsOfUse}>Terms of Use</a>
+          <div className="wrapper">
+            <div className="information">
+              <div className="container delivery">
+                <p>Delivery</p>
+                <p>
+                  {data.delivery} - {data.deliveryFee}
+                </p>
+                <p>Tickets available by {data.deliveryDate}</p>
+                <p>
+                  These mobile tickets will be transfered directly to you from a
+                  trusted seller. We'll email your instructions on how to accept
+                  them on the original ticket provider's mobile app.
+                </p>
+              </div>
+              <div className="container payment">
+                <div>
+                  <input type="radio" value="Visa" name="cardInfo" />
+                  <p>Visa - {data.creditCardNumber}</p>
+                  <p>
+                    {data.creditCardName} | exp. {data.creditCardExpiration}
+                  </p>
+                  <span>Edit | Delete</span>
+                  <p>Security Code</p>
+                  <input type="password" name="password" />
+                  <p>3-digits on back of card</p>
+                </div>
+                <div>Add New Card</div>
+                <div>
+                  <p>Or Pay With</p>
+                  <div>
+                    By using a digital wallet and continuing past this page, you
+                    have read and are accepting the{" "}
+                    <a href={data.termsOfUse}>Terms of Use</a>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="total">
-            <div>
-              <p>
-                Total{" "}
-                {data.price * data.quantity +
-                  data.serviceFee * data.quantity +
-                  data.orderingFee}{" "}
-              </p>
-            </div>
-            <p>Tickets</p>
-            <div>
-              Resale Tickets: {data.price} x {data.quantity}
-              <p>{data.price * data.quantity}</p>
-            </div>
-            <p>Notes from Seller</p>
-            <p>{data.sellerNotes}</p>
-            <p>Fees</p>
-            <div>
-              Service Fee: {data.serviceFee} x {data.quantity}
-              <p>{data.serviceFee * data.quantity}</p>
-            </div>
-            <div>
-              <p>Order Processing Fee:</p>
-              <p>{data.orderingFee}</p>
-            </div>
-            <p>Delivery</p>
-            <div>
-              <p>{data.delivery}</p>
-              <p>{data.deliveryFee}</p>
-            </div>
-            <div>Cancel Order</div>
-            <div>*All Sales Final - No Refunds</div>
+            <div className="container total">
+              <div>
+                <p>
+                  Total{" "}
+                  {data.price * data.quantity +
+                    data.serviceFee * data.quantity +
+                    data.orderingFee}{" "}
+                </p>
+              </div>
+              <p>Tickets</p>
+              <div>
+                Resale Tickets: {data.price} x {data.quantity}
+                <p>{data.price * data.quantity}</p>
+              </div>
+              <p>Notes from Seller</p>
+              <p>{data.sellerNotes}</p>
+              <p>Fees</p>
+              <div>
+                Service Fee: {data.serviceFee} x {data.quantity}
+                <p>{data.serviceFee * data.quantity}</p>
+              </div>
+              <div>
+                <p>Order Processing Fee:</p>
+                <p>{data.orderingFee}</p>
+              </div>
+              <p>Delivery</p>
+              <div>
+                <p>{data.delivery}</p>
+                <p>{data.deliveryFee}</p>
+              </div>
+              <div>Cancel Order</div>
+              <div>*All Sales Final - No Refunds</div>
 
-            <form>
-              <input
-                type="checkbox"
-                name="terms"
-                onChange={handleTermsChange}
-                required
-              />
-              <label htmlFor="terms">I have read and agree to the current <a href={data.termsOfUse}>Terms of Use</a></label>
-              <button type="submit">Submit</button>
-            </form>
+              <form>
+                <input
+                  type="checkbox"
+                  name="terms"
+                  onChange={handleTermsChange}
+                  required
+                />
+                <label htmlFor="terms">
+                  I have read and agree to the current{" "}
+                  <a href={data.termsOfUse}>Terms of Use</a>
+                </label>
+                <button type="submit">Place Order</button>
+              </form>
+            </div>
           </div>
         </>
       ) : (
