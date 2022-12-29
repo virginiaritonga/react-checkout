0d5ba53d13a2d97bc97147c3cd2e582fd3576fc7
Virginia Ritonga <virginia.p.m@mail.ugm.ac.id>
2022-12-30 06:09:50 +0700

    feat: create new checkout page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Checkout from './components/Checkout'
+
+function App() {
+  return (
+    <div className="App">
+      <Checkout/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Checkout.jsx
@@ -0,0 +1,114 @@
+import React, { useState, useEffect } from "react";
+
+function Checkout() {
+  const [data, setData] = useState(null);
+  const [termsAccepted, setTermsAccepted] = useState(false);
+
+  function handleTermsChange(event) {
+    setTermsAccepted(event.target.checked);
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        "https://6398216bfe03352a94c5a9f4.mockapi.io/api/v1/checkout/1"
+      );
+      const data = await response.json();
+      setData(data);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {data ? (
+        <>
+          <div className="delivery">
+            <p>Delivery</p>
+            <p>
+              {data.delivery} - {data.deliveryFee}
+            </p>
+            <p>Tickets available by {data.deliveryDate}</p>
+            <p>
+              These mobile tickets will be transfered directly to you from a
+              trusted seller. We'll email your instructions on how to accept
+              them on the original ticket provider's mobile app.
+            </p>
+          </div>
+
+          <div className="payment">
+            <div>
+              <input type="radio" value="Visa" name="cardInfo" />
+              <p>Visa - {data.creditCardNumber}</p>
+              <p>
+                {data.creditCardName} | exp. {data.creditCardExpiration}
+              </p>
+              <span>Edit | Delete</span>
+              <p>Security Code</p>
+              <input type="password" name="password" />
+              <p>3-digits on back of card</p>
+            </div>
+            <div>Add New Card</div>
+            <div>
+              <p>Or Pay With</p>
+              <div>
+                By using a digital wallet and continuing past this page, you
+                have read and are accepting the{" "}
+                <a href={data.termsOfUse}>Terms of Use</a>
+              </div>
+            </div>
+          </div>
+
+          <div className="total">
+            <div>
+              <p>
+                Total{" "}
+                {data.price * data.quantity +
+                  data.serviceFee * data.quantity +
+                  data.orderingFee}{" "}
+              </p>
+            </div>
+            <p>Tickets</p>
+            <div>
+              Resale Tickets: {data.price} x {data.quantity}
+              <p>{data.price * data.quantity}</p>
+            </div>
+            <p>Notes from Seller</p>
+            <p>{data.sellerNotes}</p>
+            <p>Fees</p>
+            <div>
+              Service Fee: {data.serviceFee} x {data.quantity}
+              <p>{data.serviceFee * data.quantity}</p>
+            </div>
+            <div>
+              <p>Order Processing Fee:</p>
+              <p>{data.orderingFee}</p>
+            </div>
+            <p>Delivery</p>
+            <div>
+              <p>{data.delivery}</p>
+              <p>{data.deliveryFee}</p>
+            </div>
+            <div>Cancel Order</div>
+            <div>*All Sales Final - No Refunds</div>
+
+            <form>
+              <input
+                type="checkbox"
+                name="terms"
+                onChange={handleTermsChange}
+                required
+              />
+              <label htmlFor="terms">I have read and agree to the current <a href={data.termsOfUse}>Terms of Use</a></label>
+              <button type="submit">Submit</button>
+            </form>
+          </div>
+        </>
+      ) : (
+        <div>Loading data...</div>
+      )}
+    </div>
+  );
+}
+
+export default Checkout;
